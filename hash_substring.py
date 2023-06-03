2b87bac08afd883d9667088210af98e3a2b3c43a
Kkris1225 <124594712+Kkris1225@users.noreply.github.com>
2023-06-03 04:55:23 +0000

    first commit

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,54 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    input_type = input().rstrip()
+
+    if input_type == "I":
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == "F":
+        filename = "tests/06"
+        with open(filename, 'r', encoding="utf-8") as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+
+    return pattern, text
+
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
+
+def get_hash(string):
+    Q, B = 256, 13
+    length = len(string)
+    string_hash = 0
+    for i in range(length):
+        unicode_value = ord(string[i])
+        string_hash = (B * string_hash + unicode_value) % Q
+    return string_hash
+
+
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    Q, B = 256, 13
+    pattern_length, text_length = len(pattern), len(text)
+    multiplier = 1
+    for i in range(1, pattern_length):
+        multiplier = (multiplier * B) % Q
+    pattern_hash = get_hash(pattern)
+    text_hash = get_hash(text[:pattern_length])
+    positions = []
+    for i in range(text_length - pattern_length + 1):
+        if pattern_hash == text_hash and pattern == text[i:i + pattern_length]:
+            positions.append(str(i))
+        if i < text_length - pattern_length:
+            first_char_unicode = ord(text[i])
+            last_char_unicode = ord(text[i + pattern_length])
+            text_hash = ((text_hash - first_char_unicode * multiplier) * B + last_char_unicode) % Q
+    return positions
 
-    # and return an iterable variable
-    return [0]
 
-
-# this part launches the functions
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    occurrences = get_occurrences(*read_input())
+    print_occurrences(occurrences)
 
